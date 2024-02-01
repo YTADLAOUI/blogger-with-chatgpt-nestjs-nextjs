023f055633979d00ff8cd0375f24aa90583cbc61
youssef tadlaoui <usftadlaoui@gmail.com>
2024-02-01 05:11:04 +0100

    connect app with mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { AuthModule } from './auth/auth.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [],
+  imports: [AuthModule,MongooseModule.forRootAsync({
+    useFactory: () => ({
+      uri: process.env.MONGODB_URI,
+    }),
+  }),
+  ConfigModule.forRoot(),],
   controllers: [AppController],
   providers: [AppService],
 })
