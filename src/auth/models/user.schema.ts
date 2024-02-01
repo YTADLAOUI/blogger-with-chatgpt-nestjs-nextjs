943e0966776000b12226ceac717d7d2bba7ace09
youssef tadlaoui <usftadlaoui@gmail.com>
2024-02-01 05:12:24 +0100

    add schema user

---
+++ src/auth/models/user.schema.ts
@@ -0,0 +1,23 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type UserDocument = HydratedDocument<User>;
+
+@Schema()
+export class User {
+
+    @Prop({ required: true })
+    username: string;
+    
+    @Prop({ required: true , unique: true ,isemail: true})
+    email: string;
+    
+    @Prop({ required: true })
+    password: string;
+    
+    @Prop({enum: ['admin'], default: 'admin'})
+    role: string;
+
+}
+
+export const userSchema = SchemaFactory.createForClass(User);
