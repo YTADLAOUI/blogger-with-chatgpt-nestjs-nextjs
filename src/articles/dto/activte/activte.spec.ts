2ac0c17e86da3a8a1cb5c819ce332f4c0157d94d
youssef tadlaoui <usftadlaoui@gmail.com>
2024-03-06 09:16:08 +0100

    Creation articles resources and crud

---
+++ src/articles/dto/activte/activte.spec.ts
@@ -0,0 +1,7 @@
+import { Activte } from './activte';
+
+describe('Activte', () => {
+  it('should be defined', () => {
+    expect(new Activte()).toBeDefined();
+  });
+});
---
+++ src/articles/dto/activte/activte.ts
@@ -0,0 +1,6 @@
+export class Activte {
+  total_likes: number;
+  total_comments: number;
+  total_reads: number;
+  total_parent_comments: number;
+}
