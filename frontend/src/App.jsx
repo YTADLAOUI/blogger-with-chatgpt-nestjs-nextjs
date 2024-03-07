d946801b0ea97f9aa61e75b565a416d3cc672c49
youssef tadlaoui <usftadlaoui@gmail.com>
2024-03-07 11:21:57 +0100

    Setting Up Routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,19 @@
 import { useState } from 'react'
 import './App.css'
 import Navbar from './components/navbar'
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
   
 
   return (
     <>
+    <Routes>
+      <Route path='/' element={Navbar}/>
+      <Route path='/signin' element={<h1>Sign in page</h1>}/>
+      <Route path='/signup' element={<h1>Sing up page</h1>}/>
+
+    </Routes>
     <Navbar />
     </>
   )
