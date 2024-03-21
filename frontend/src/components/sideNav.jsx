056d1722bc9b08761654fd84b9041eecbf222bfd
youssef tadlaoui <usftadlaoui@gmail.com>
2024-03-21 00:45:47 +0000

    add changePassword page

--- frontend/src/components/sideNav.jsx
+++ frontend/src/components/sideNav.jsx
@@ -4,17 +4,25 @@ import { NavLink, Navigate, Outlet } from 'react-router-dom'
 const sideNav = () => {
   let user = JSON.parse(localStorage.getItem('user'));
   let [page,setPage]=useState("")
+  let [show,setShow]=useState(true)
+  console.log(show)
   return (
     // !user.isLogin ? <Navigate to='/signin'/>:
     <>
      <section className='relative bg-green flex gap-10 py-0 m-0 max-md:flex-col'>
     <div className='sticky top-[80px] z-30'>
-      <div className='min-w-[200px] h-cover md:sticky top-24 overflow-y-auto p-6 md:pr-0'>
+      <div className='md:hidden bg-white py-1 border-b border-grey flex flex-nowrap overflow-x-auto'>
+        <button onClick={()=>setShow(!show)} className='p-5 capitalize'>
+          <i className='fi fi-rr-menu-dots text-2xl'></i>
+        </button>
+        
+      </div>
+      {show ? <div className={'min-w-[200px] h-cover md:sticky top-24 overflow-y-auto  p-6 md:pr-0 md:border-grey md:border-r absolute max-md:top[64px] bg-white  max-md:px-16 max-md:-ml-7 max-md:w-[calc(100%+80px)]'}>
           <h1 className='text-xl text-dark-grey mb-3'>
             Dashboard
             </h1>
             <hr className='border-grey -ml-6 mb-8 mr-6' />
-            <div className='flex flex-col gap-8'>
+        
 
             <NavLink to="/" className='sidebar-link' onClick={(e)=>setPage(e.target.innerText)}>
               <i className='fi fi-rr-document me-3' ></i>
@@ -28,12 +36,12 @@ const sideNav = () => {
               <i className='fi fi-rr-file-edit me-3' ></i>
               Write
               </NavLink>
-            </div>
+          
             <h1 className='text-xl text-dark-grey mb-3 mt-8'>
             Settings
             </h1>
             <hr className='border-grey -ml-6 mb-8 mr-6' />
-            <div className='flex flex-col gap-8'>
+            
             <NavLink to="/settings/edit-profile" className='sidebar-link' onClick={(e)=>setPage(e.target.innerText)}>
               <i className='fi fi-rr-user me-3' ></i>
               Edit Profile
@@ -42,14 +50,12 @@ const sideNav = () => {
               <i className='fi fi-rr-lock me-3' ></i>
               Change Password
               </NavLink>
-
-              </div>
-  
-        </div>  
+        </div>:<></> } 
       </div>
-
-     </section>
+    <div className='max-md:mt-8 mt-5 w-full'>
       <Outlet/>
+    </div>
+     </section>
     </>
   )
 }
--- frontend/src/pages/changePassword.jsx
+++ frontend/src/pages/changePassword.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import InputBox from '../components/inputBox'
 const changePassword = () => {
+  const [currentPassword,setCurrentPassword]=useState('')
+  const [newPassword,setNewPassword]=useState('')
+  console.log(currentPassword,newPassword)
   return (
-    <div>changePassword</div>
+    <from>
+<h1 className='text-4xl text-dark-grey mb-10'>Change Password</h1>
+    <div className='py-10 w-full md:max-w-[400px] m-auto'>
+    <InputBox name="password" type="password" className="profile-edit-input" placeholder="currentPassword"
+    icon="fi-rr-unlock"
+    onChange={(e)=>setCurrentPassword(e.target.value)}
+    />
+    <InputBox name="password" type="password" className="profile-edit-input" placeholder="New Password" icon="fi-rr-unlock"
+    onChange={(e)=>setNewPassword(e.target.value)}
+    />
+
+    <button className='btn-dark px-10'>Change Password</button>
+    </div>
+    </from>
   )
 }
 
